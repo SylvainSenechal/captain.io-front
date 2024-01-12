84d6109fbc10f5fd82a614cd8e6c88c7f7ea82b4
Sylvain Senechal <senechalsylvain@hotmail.com>
2024-01-13 04:56:05 +0800

    fix import request.js

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { useState, useEffect, useRef } from 'react'
 import './App.css'
-import { get, put } from "./utils/requests.js";
+import { get, put } from "./utils/Requests.js";
 
 const envData = {
   apiURL:
